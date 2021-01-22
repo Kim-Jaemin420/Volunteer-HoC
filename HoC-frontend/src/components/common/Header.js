98f008320ac53139b9983f1f02477e3a68205b57
Kim-Jaemin420 <jany240@naver.com>
2021-01-22 17:41:41 +0900

    last

--- HoC-frontend/src/components/common/Header.js
+++ HoC-frontend/src/components/common/Header.js
@@ -127,18 +127,20 @@ const Header = () => {
               로그아웃
             </Button>
           ) : (
-            <Button className="headerButton" as={Link} to="/login">
-              로그인
-            </Button>
+            <>
+              <Button className="headerButton" as={Link} to="/login">
+                로그인
+              </Button>
+              <Button
+                style={{ marginLeft: 10 }}
+                className="headerButton"
+                as={Link}
+                to="/register"
+              >
+                회원가입
+              </Button>
+            </>
           )}
-          <Button
-            style={{ marginLeft: 10 }}
-            className="headerButton"
-            as={Link}
-            to="/register"
-          >
-            회원가입
-          </Button>
         </div>
       </Wrapper>
       <Spacer />
